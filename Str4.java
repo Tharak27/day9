ba72da04e2872c8ed1e3a6ecf5a0b1f8406ba09b
Tharak Ram <tharakarame@gmail.com>
2024-12-10 12:19:28 +0530

    Add files via upload

---
+++ Str4.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Str4{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a=sc.nextLine();
+        String sp[]=a.split(" ");
+        System.out.println(Arrays.toString(sp));
+    }
+}
